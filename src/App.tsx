717067bcc077c164cefcae6530b28c30f64013a2
Dylan <dylan.@alMacBook-Pro-alkhas-bDylan.local>
2023-05-29 23:12:46 +0300

    w

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Layout from "./components/user/laoyt";
+import HomeContainer from "./containers/user/home-continer";
+
+
+function App() {
+  return (
+    <Layout >
+      <HomeContainer />
+    </Layout>
+  );
+}
+
+export default App;
